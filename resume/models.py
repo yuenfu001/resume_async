0c514ec53304c9102a3fc58407e10f4164f71c82
yuenfu001 <liuyuenfu@gmail.com>
2024-06-04 13:30:12 +0100

    added project app and drf

--- resume/models.py
+++ resume/models.py
@@ -2,13 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
-class Resume(User):
+class Resume(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     company = models.CharField(max_length=50)
     school = models.CharField(max_length=50)
     college = models.CharField(max_length=50)
     achievement = models.TextField()
     hobbies = models.TextField()
 
+    class Meta:
+        verbose_name = "Resume"
+        verbose_name_plural = "Resume"
     def __str__(self):
         return f"{self.company}"
     
