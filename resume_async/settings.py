147cfa485ea9e4017f37c62a18ab34af736db95e
yuenfu001 <liuyuenfu@gmail.com>
2024-06-06 16:55:35 +0100

    added host

--- resume_async/settings.py
+++ resume_async/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config("KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("BUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','*']
 
 
 # Application definition
